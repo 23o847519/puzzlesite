a906f9e2ce04853d349f611fcc6089a9f72ce1ff
23840932 <violetpayne11@gmail.com>
2023-04-24 22:08:59 -0500

    a
    a

---
+++ welcome.js
@@ -0,0 +1,31 @@
+var lines = [
+    "Admirable, mortal. You have proven yourself worthy of traversing the treacherous labyrinth of reasoning that lies ahead. But be warned, what awaits you is a reward shrouded in darkness, a prize coveted by many, but grasped by few.",
+    "Be mindful, for our twisted machinations are not easily unraveled.",
+    "Proceed with caution, for in our realm, logic is the key, and failure is met with unspeakable consequences."
+  ];
+  
+  var lineNum = 0;
+  var linePos = 0;
+  var interval = setInterval(writeLine, 1);
+  
+  function writeLine() {
+    if (lineNum < lines.length) {
+      var lineId = "line" + (lineNum + 1);
+      var lineElem = document.getElementById(lineId);
+      lineElem.innerHTML += lines[lineNum][linePos];
+      linePos++;
+      if (linePos >= lines[lineNum].length) {
+        lineNum++;
+        linePos = 0;
+      }
+    } else {
+      clearInterval(interval);
+      document.getElementById("puzzle-btn").style.display = "block";
+    }
+  }
+  
+  var puzzleBtn = document.getElementById("puzzle-btn");
+  puzzleBtn.addEventListener("click", function() {
+    window.location.href = "puzzle.html";
+  });
+  
